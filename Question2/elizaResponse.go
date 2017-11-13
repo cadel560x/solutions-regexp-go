69841bbdb624df2f5358fdda83b92068bd8fa851
JAVIER MANTILLA - Student <G00329649@gmit.ie>
2017-11-13 10:42:22 +0000

    Question2 initial commit

---
+++ Question2/elizaResponse.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"regexp"
+	"time"
+)
+
+func ElizaResponse(input string) string {
+
+	if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, input); matched {
+		return "Why don’t you tell me more about your father?"
+	}
+
+	responses := []string{
+		"I’m not sure what you’re trying to say. Could you explain it to me?",
+		"How does that make you feel?",
+		"Why do you say that?",
+	}
+
+	randindex := rand.Intn(len(responses))
+
+	return responses[randindex]
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	fmt.Println("People say I look like both my mother and father.")
+	fmt.Println(ElizaResponse("People say I look like both my mother and father."))
+	fmt.Println()
+
+	fmt.Println("Father was a teacher.")
+	fmt.Println(ElizaResponse("Father was a teacher."))
+	fmt.Println()
+
+	fmt.Println("I was my father’s favourite.")
+	fmt.Println(ElizaResponse("I was my father’s favourite."))
+	fmt.Println()
+
+	fmt.Println("I’m looking forward to the weekend.")
+	fmt.Println(ElizaResponse("I’m looking forward to the weekend."))
+	fmt.Println()
+
+	fmt.Println("My grandfather was French!")
+	fmt.Println(ElizaResponse("My grandfather was French!"))
+	fmt.Println()
+}
